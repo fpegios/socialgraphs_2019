36596c7cdae0497311e1713acc190d68298b9bac
Fotis Pegios <fotispeg@gmail.com>
2019-12-08 15:12:50 +0200

    create website structure

---
+++ assignment2/website/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+html, body, #app {
+  height: 100%;
+}
+
+.tabs .card-header {
+  position: fixed!important;
+}
+
+.tab-content {
+  margin-left: 200px;
+}
+
+</style>
---
+++ assignment2/website/src/components/Index.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-container class="p-0 h-100" fluid>
+    <b-row class="h-100" no-gutters>
+      <b-col>
+        <b-tabs class="h-100" pills card vertical align="center">
+          <b-tab title="Introduction" active>
+            <Introduction />
+          </b-tab>
+          <b-tab title="Dataset">
+            <Dataset />
+          </b-tab>
+          <b-tab title="Network Analysis">
+            <NetworkAnalysis />
+          </b-tab>
+          <b-tab title="Conclusion">
+            <Conclusion />
+          </b-tab>
+        </b-tabs>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Introduction from './Introduction.vue'
+import Dataset from './Dataset.vue'
+import NetworkAnalysis from './NetworkAnalysis.vue'
+import Conclusion from './Conclusion.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Introduction,
+    Dataset,
+    NetworkAnalysis,
+    Conclusion
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
